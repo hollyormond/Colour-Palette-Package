1a8f2556c664f59c8531d0dae7810e1af178994d
Holly Ormond <56247458+hollyormond@users.noreply.github.com>
2023-11-06 12:42:44 +1300

    Added files, skeleton of colour palette package, R

---
+++ colslr/R/colslr.R
@@ -0,0 +1,107 @@
+# Colour palette for SLR Consulting
+#
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+primary_green = c(
+  green1 = '#EEF7DB',
+  green2 = '#D6F591',
+  green3 = '#A9C272',
+  green4 = '#667545',
+  green5 = '#3C533C',
+  green6 = '#263326'
+)
+
+shades = c(
+  shade1 = '#F6F6F2',
+  shade2 = '#EBECE7',
+  shade3 = '#D4D9D2',
+  shade4 = '#BEC5BC',
+  shade5 = '#51544A',
+  shade6 = '#1E1E1E'
+)
+
+secondary_palette = c(
+  cyan1 = '#DDF3F5',
+  cyan2 = '#C4F0F5',
+  cyan3 = '#ABEEF5',
+  cyan4 = '#92ECF5',
+  cyan5 = '#679EA3',
+  cyan6 = '#3D5254',
+  blue1 = '#DDE4F5',
+  blue2 = '#C4D4F5',
+  blue3 = '#ABC3F5',
+  blue4 = '#92B2F5',
+  blue5 = '#6779A3',
+  blue6 = '#3D4454',
+  lilac1 = '#EFDDF5',
+  lilac2 = '#E7C4F5',
+  lilac3 = '#E0ABF5',
+  lilac4 = '#D993F5',
+  lilac5 = '#9167A3',
+  lilac6 = '#4E3D54',
+  pink1 = '#F5DDE6',
+  pink2 = '#F5C4D7',
+  pink3 = '#F5ABC7',
+  pink4 = '#F593B7',
+  pink5 = '#A3677E',
+  pink6 = '#523B44',
+  apricot1 = '#F5E0DD',
+  apricot2 = '#F5CCC4',
+  apricot3 = '#F5B7AB',
+  apricot4 = '#F5A293',
+  apricot5 = '#A36F67',
+  apricot6 = '#54403D',
+  sand1 = '#F5E9DD',
+  sand2 = '#F5DDC4',
+  sand3 = '#F5D1AB',
+  sand4 = '#F5C492',
+  sand5 = '#A38667',
+  sand6 = '#54493D',
+  green1 = '#EEF7DB',
+  green2 = '#D6F591',
+  green3 = '#A9C272',
+  green4 = '#667545',
+  green5 = '#3C533C',
+  green6 = '#263326'
+)
+
+secondary_light = c(
+  cyan1 = '#DDF3F5',
+  blue3 = '#ABC3F5',
+  lilac2 = '#E7C4F5',
+  pink2 = '#F5C4D7',
+  apricot1 = '#F5E0DD',
+  sand3 = '#F5D1AB',
+  green2 = '#D6F591'
+)
+
+secondary_mid = c(
+  cyan4 = '#92ECF5',
+  blue4 = '#92B2F5',
+  lilac4 = '#D993F5',
+  pink4 = '#F593B7',
+  apricot4 = '#F5A293',
+  sand4 = '#F5C492',
+  green4 = '#667545'
+)
+
+secondary_dark = c(
+  cyan5 = '#679EA3',
+  cyan6 = '#3D5254',
+  blue5 = '#6779A3',
+  lilac6 = '#4E3D54',
+  pink5 = '#A3677E',
+  apricot5 = '#A36F67',
+  sand5 = '#A38667',
+  green5 = '#3C533C'
+)
+
